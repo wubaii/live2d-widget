5c900cba1a476db59b4a30ce503008280e657859
wubaii <33111877+wubaii@users.noreply.github.com>
2020-02-04 20:01:15 +0800

    new live2d widget

--- waifu-tips.js
+++ waifu-tips.js
@@ -152,7 +152,7 @@ function loadWidget(waifuPath, apiPath) {
 		if (modelId == null) {
 			// 首次访问加载 指定模型 的 指定材质
 			var modelId = 1, // 模型 ID
-				modelTexturesId = 53; // 材质 ID
+				modelTexturesId = 17; // 材质 ID
 		}
 		loadModel(modelId, modelTexturesId);
 		fetch(waifuPath)
